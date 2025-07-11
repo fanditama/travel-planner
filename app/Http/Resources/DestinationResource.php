a27db5af16aed72f1f0825da4c958ac6126b3355
Affandi Pratama <afandip911@gmail.com>
2025-07-11 13:22:51 +0700

    Tambah resource untuk data destinasi

---
+++ app/Http/Resources/DestinationResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DestinationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'location' => $this->location,
+            'latitude' => $this->latitude,
+            'longitude' => $this->longitude,
+            'category' => $this->category,
+            'average_rating' => $this->average_rating,
+            'image_url' => $this->image_url,
+            'approx_price_range' => $this->approx_price_range,
+            'best_time_to_visit' => $this->best_time_to_visit,
+        ];
+    }
+}
