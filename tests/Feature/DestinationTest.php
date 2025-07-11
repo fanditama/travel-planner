e08e7668eef44b633e3902e744400bf5dbd4a506
Affandi Pratama <afandip911@gmail.com>
2025-07-11 16:56:53 +0700

    Tambah skenario test saat create data destinasi baru gagal

--- tests/Feature/DestinationTest.php
+++ tests/Feature/DestinationTest.php
@@ -36,4 +36,30 @@ class DestinationTest extends TestCase
                 ]
             ]);
     }
+
+    public function testCreateFailed()
+    {
+        $this->post('/api/destinations',
+            [
+                'name' => '',
+                'location' => '',
+                'latitude' => -6.2088,
+                'longitude' => 106.8456,
+                'category' => 'Kuliner',
+                'average_rating' => 4.5,
+                'image_url' => 'https://example.com/image.jpg',
+                'approx_price_range' => 'Terjangkau',
+                'best_time_to_visit' => 'April hingga Oktober',
+            ])->assertStatus(400)
+            ->assertJson([
+                'errors' => [
+                    'name' => [
+                        'The name field is required.'
+                    ],
+                    'location' => [
+                        'The location field is required.'
+                    ]
+                ]
+            ]);
+    }
 }
